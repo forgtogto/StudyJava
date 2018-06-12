9652142125305c7139747cb4d624ecebf321c944
JeongkwonSeo <forgtogto@gmail.com>
2018-06-12 12:57:59 +0900

    FX lightSystem fix.

--- JavaStudy/src/fx_lightSystem/house_main.java
+++ JavaStudy/src/fx_lightSystem/house_main.java
@@ -6,24 +6,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
- 
+
 public class house_main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		 	Parent root = FXMLLoader.load(getClass().getResource("view/mainpage.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setTitle("Controll My House ver0.01");
-			primaryStage.setWidth(710);       
-			primaryStage.setHeight(470);      
-			primaryStage.setResizable(false);   
-			primaryStage.setScene(scene);
-			primaryStage.show();
- 	}
-	
+		Parent root = FXMLLoader.load(getClass().getResource("view/mainpage.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Controll My House ver0.01");
+		primaryStage.setWidth(710);
+		primaryStage.setHeight(470);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
-		
+
 	}
-	
-	
+
 }
